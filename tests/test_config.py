328c957ea416c4c69e4f61ba3147e9f566781788
David M Noriega <davidmnoriega@gmail.com>
2016-04-18 23:28:23 -0700

    Initial test skel

---
+++ tests/test_config.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class ConfigParserTest(unittest.TestCase):
+    def test_config(self):
+        pass
+
+    def test_config_ssl(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
