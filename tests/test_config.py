92d20099baf27aa52953b297480d140d8dfc5fdf
David M Noriega <davidmnoriega@gmail.com>
2016-04-24 21:30:56 -0700

    Adding first pass at tests for config parsing

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,12 +1,20 @@
 import unittest
+from ldap3 import Server, Connection, Tls
+from gh_org.config import Config
 
 
 class ConfigParserTest(unittest.TestCase):
     def test_config(self):
-        pass
+        config = Config('./tests/test_config.ini')
+        self.assertIsInstance(config.server, Server)
+        self.assertIsInstance(config.conn, Connection)
+        self.assertIsNotInstance(config.tls, Tls)
 
     def test_config_ssl(self):
-        pass
+        config = Config('./tests/test_config_ssl.ini')
+        self.assertIsInstance(config.server, Server)
+        self.assertIsInstance(config.conn, Connection)
+        self.assertIsInstance(config.tls, Tls)
 
 if __name__ == '__main__':
     unittest.main()
