422b595e68b5241936a8f8af44e756ec00abda54
David M Noriega <davidmnoriega@gmail.com>
2016-04-24 21:32:47 -0700

    Adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
     version='0.1',
     packages=['gh_org'],
     entry_points={
-        'console_scripts': ['gh_org=gh_org.cli:main']}
+        'console_scripts': ['gh_org=gh_org.cli:main']},
+    install_requires=['ldap3'],
     )
