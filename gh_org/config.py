73b3357dae178750142b2f6de67aa374b2821122
David M Noriega <davidmnoriega@gmail.com>
2016-04-27 23:24:50 -0700

    Improved tests and Config class to pass said tests

--- gh_org/config.py
+++ gh_org/config.py
@@ -1,3 +1,4 @@
+from ssl import CERT_NONE, CERT_REQUIRED
 from ldap3 import Server, Connection, Tls
 try:
     from ConfigParser import SafeConfigParser, NoOptionError
@@ -8,7 +9,15 @@ except ImportError:
 class Config(object):
     def __init__(self, configfile):
         self._parser = SafeConfigParser()
-        self._parser.read(configfile)
+        if isinstance(configfile, basestring):
+            self._parser.read(configfile)
+        else:
+            self._parser.readfp(configfile)
+
+        if self._parser.has_option('ldap', 'use_tls'):
+            self.use_tls = self._parser.getboolean('ldap', 'use_tls')
+        else:
+            self.use_tls = False
 
         self.tls = self._tls_config()
         self.server = self._server_config()
@@ -16,19 +25,40 @@ class Config(object):
 
     def _tls_config(self):
         if self._parser.has_section('tls'):
-            pass
+            try:
+                if self._parser.getboolean('tls', 'validate'):
+                    validate = CERT_REQUIRED
+                else:
+                    validate = CERT_NONE
+            except NoOptionError:
+                validate = CERT_REQUIRED
+
+            try:
+                ca_file = self._parser.get('tls', 'ca_file')
+            except NoOptionError:
+                ca_file = None
+
+            return Tls(validate=validate, ca_certs_file=ca_file)
         else:
-            return None
+            return Tls(validate=CERT_REQUIRED)
 
     def _server_config(self):
-        if self._parser.has_item('ldap', 'uri'):
+        if self._parser.has_option('ldap', 'uri'):
             uri = self._parser.get('ldap', 'uri')
         else:
             raise NoOptionError
 
-        if self._parser.has_item('ldap', 'port'):
-            port = self._parser.has_item('ldap', 'port')
+        if self._parser.has_option('ldap', 'port'):
+            port = self._parser.has_option('ldap', 'port')
         else:
             port = None
 
         return Server(host=uri, port=port, tls=self.tls)
+
+    def _conn_config(self):
+        try:
+            username = self._parser.get('ldap', 'binddn')
+            password = self._parser.get('ldap', 'bindpw')
+            return Connection(self.server, username, password)
+        except NoOptionError:
+            raise NoOptionError('Something wrong with your config file')
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,20 +1,33 @@
 import unittest
+from StringIO import StringIO
 from ldap3 import Server, Connection, Tls
 from gh_org.config import Config
+# Inspired from Python's own unit tests for ConfigParser
+# See https://hg.python.org/cpython-fullhistory/file/tip/Lib/test/test_cfgparser.py
 
 
 class ConfigParserTest(unittest.TestCase):
-    def test_config(self):
-        config = Config('./tests/test_config.ini')
-        self.assertIsInstance(config.server, Server)
-        self.assertIsInstance(config.conn, Connection)
-        self.assertIsNotInstance(config.tls, Tls)
+    def fromstring(self, string):
+        sio = StringIO(string)
+        cf = Config(sio)
+        return cf
 
-    def test_config_ssl(self):
-        config = Config('./tests/test_config_ssl.ini')
+    def test_config(self):
+        config_string = (
+            "[ldap]\n"
+            "uri = ldap://ipa.demo1.freeipa.org\n"
+            "binddn = cn=admin,dc=demo1,dc=freeipa,dc=org\n"
+            "bindpw = Secret123\n"
+            "basedn = dc=demo1,dc=freeipa,dc=org\n"
+            )
+        config = self.fromstring(config_string)
         self.assertIsInstance(config.server, Server)
         self.assertIsInstance(config.conn, Connection)
         self.assertIsInstance(config.tls, Tls)
+
+    def test_config_read_file(self):
+        config = Config('./tests/test_config.ini')
+        self.assertIsInstance(config, Config)
 
 if __name__ == '__main__':
     unittest.main()
